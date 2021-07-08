509a5310c3430b9ba62f74a234891a0f81dc7465
apinklover <61984863+apinklover@users.noreply.github.com>
2021-07-08 21:47:39 +0900

    Update main.py
    Removed some hardcoded variables

--- main.py
+++ main.py
@@ -3,35 +3,45 @@ import keyboard
 import time as t
 
 mouse = Controller()
+halt_key = "ctrl"
+num_items_per_hotbar = 64
 
 def click(button, for_how_long):
     mouse.press(button)
     t.sleep(for_how_long)
     mouse.release(button)
 
+
 def place():
     mouse.press(B.right)
     mouse.release(B.right)
 
+
 def mine():
-    click(B.left, 0.7)
+    click(B.left, 0.75)
+
 
 def move_down_hotbar(num):
     mouse.scroll(0, num)
 
+
 def move_up_hotbar(num):
     mouse.scroll(0, -num)
 
+
 def mine_concrete():
-    for hotbar in range(2, 10):
-        for i in range(64):
-            if keyboard.is_pressed("ctrl"):
-                exit(69)
+    for hotbar in range(2, 11):
+        for i in range(num_items_per_hotbar + 3):      # mines a bit more to account for missed blocks due to lag
+            if keyboard.is_pressed(halt_key):
+                exit(0)
             place()
             t.sleep(0.05)
             mine()
             t.sleep(0.05)
-        keyboard.press_and_release(f"{hotbar}")
+        try:
+            keyboard.press_and_release(f"{hotbar}")
+        except ValueError:
+            pass
         t.sleep(0.05)
         keyboard.press_and_release("F")
         t.sleep(0.05)
